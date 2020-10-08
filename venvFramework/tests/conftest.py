8f71209c24efb990351ce791fc4bff6f7f89c608
yagogomes792 <yago.792@hotmail.com>
2020-10-08 11:17:37 -0300

    adicionado função driver no baseclass

--- venvFramework/tests/conftest.py
+++ venvFramework/tests/conftest.py
@@ -5,8 +5,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
+#how to choose browser in the command line
+#def pytest_addoption(parser):
+#   parser.addoption(
+#       '--browser_name', action='store', default='chrome')
+
 @pytest.fixture(scope='class')
 def setBrowser(request):
+    #browser_name = request.config.getOption('browser_bame')
+    #if browser_name == chrome:
+    #execute the desired browser
     driver = webdriver.Chrome()
     driver.get('https://rahulshettyacademy.com/angularpractice/')
     #utilizado instância 'request' para habilitar a variavel de classe 'driver'
--- venvFramework/utilities/BaseClass.py
+++ venvFramework/utilities/BaseClass.py
@@ -3,9 +3,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
+
 #Classe base para ser usada como herança nos testes
 @pytest.mark.usefixtures('setBrowser')
 class BaseClass:
+
+    def __init__(self, driver):
+        self.driver = driver
     
     def verifyLinkPresence(self, text):
         wait = WebDriverWait(self.driver, 7)
